e6b5aa57a8940c1633401328c3ce50b3e1bc90df
Shriman <shrimanpro.gamer@gmail.com>
2025-11-28 23:54:02 -0700

    feat: switched to raw mode, reads byte by byte to program

--- shrimp.c
+++ shrimp.c
@@ -1,6 +1,31 @@
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+struct termios orig_termios;
+
+void disableRawMode()
+{
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawmode()
+{
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main()
 {
+  enableRawmode();
+  
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
