4b577faabf41d0f7e9797f8858742e3586c06936
Shriman <shrimanpro.gamer@gmail.com>
2025-11-29 00:33:54 -0700

    feat: disabled control keys, they won't do anything now

--- shrimp.c
+++ shrimp.c
@@ -17,7 +17,9 @@ void enableRawmode()
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -27,6 +29,16 @@ int main()
   enableRawmode();
   
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\r\n", c);
+    }
+    else 
+    {
+      printf("%d ('%c')\r\n", c, c);
+    }
+  }
   return 0;
 }
