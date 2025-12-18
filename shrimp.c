6f29201483124087724c1d60ede1ef6d7ba0d824
Shriman <shrimanpro.gamer@gmail.com>
2025-12-17 17:53:11 -0700

    feat: added timeout for read

--- shrimp.c
+++ shrimp.c
@@ -17,9 +17,12 @@ void enableRawmode()
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -28,9 +31,10 @@ int main()
 {
   enableRawmode();
   
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+  while (1)
   {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
     if (iscntrl(c))
     {
       printf("%d\r\n", c);
@@ -38,6 +42,10 @@ int main()
     else 
     {
       printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q')
+    {
+      break;
     }
   }
   return 0;
