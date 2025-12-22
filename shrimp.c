a670001b2fc7dba1682218b103a169d1f7648dcc
Shriman <shrimanpro.gamer@gmail.com>
2025-12-22 10:41:12 -0700

    feat: added small visual

--- shrimp.c
+++ shrimp.c
@@ -68,10 +68,22 @@ char editorReadKey()
 
 }
 
+void editorDrawRows()
+{
+  int y;
+  for (y = 0; y < 24; y++)
+  {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editorRefreshScreen()
 {
   write(STDOUT_FILENO, "\x1b[2J",4);
   write(STDOUT_FILENO, "\x1b[H",3);
+
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[b", 3);
 }
 
 void editorProcessKeyPress()
