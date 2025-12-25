6af9dc11545abadd7606bcf40b9cd339602488a8
Shriman <shrimanpro.gamer@gmail.com>
2025-12-25 11:47:55 -0700

    feat: way to move cursor using 'wasd'

--- shrimp.c
+++ shrimp.c
@@ -17,6 +17,7 @@
 
 struct editorConfig
 {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -211,11 +212,34 @@ void editorRefreshScreen()
   abAppend(&ab, "\x1b[H",3);
 
   editorDrawRows(&ab);
-  abAppend(&ab, "\x1b[b", 3);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
+}
+
+void editorMoveCursor(char key)
+{
+  switch (key)
+  {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
 }
 
 void editorProcessKeyPress()
@@ -229,6 +253,12 @@ void editorProcessKeyPress()
       write(STDOUT_FILENO, "\x1b[H",3);
       exit(0);
       break;
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c);
+      break;
   }
 }
 
@@ -236,6 +266,8 @@ void editorProcessKeyPress()
 
 void initEditor()
 {
+  E.cx = 0;
+  E.cy = 0;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
   {
     die("getWindowSize");
