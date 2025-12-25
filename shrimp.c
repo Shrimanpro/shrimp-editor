3bdb420e53ee2f2d46f6b05a054c507d3ad6bbdb
Shriman <shrimanpro.gamer@gmail.com>
2025-12-24 23:42:37 -0700

    refactor: obtains window size regardless of OS

--- shrimp.c
+++ shrimp.c
@@ -74,6 +74,42 @@ char editorReadKey()
     }
   }
   return c;
+}
+
+int getCursorPosition(int *rows, int *cols)
+{
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+  {
+    return -1;
+  }
+  while (i < sizeof(buf) - 1)
+  {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+    {
+      break;
+    }
+    if (buf[i] == 'R')
+    {
+      break;
+    }
+    i++;
+  }
+  buf[i] = '\0';
+  printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+  
+  if (buf[0] != '\x1b' || buf[1] != '[')
+  {
+    return -1;
+  }
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+  {
+    return -1;
+  }
+
+  return 0;
 
 }
 
@@ -83,7 +119,11 @@ int getWindowSize(int *rows, int *cols)
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
   {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+    {
+      return -1;
+    }
+    return getCursorPosition(rows, cols);
   }
   else 
   {
