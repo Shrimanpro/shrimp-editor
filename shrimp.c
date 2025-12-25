a4f189b86525e052be2545d296496f930c1c4d30
Shriman <shrimanpro.gamer@gmail.com>
2025-12-24 23:22:47 -0700

    feat: way to get row/col of terminal to display properly

--- shrimp.c
+++ shrimp.c
@@ -1,16 +1,25 @@
 #include <asm-generic/errno-base.h>
+#include <asm-generic/ioctls.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig
+{
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 void die(const char *s)
 {
@@ -25,7 +34,7 @@ void die(const char *s)
 
 void disableRawMode()
 {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
   {
     die("tcsetattr");
   }
@@ -33,13 +42,13 @@ void disableRawMode()
 
 void enableRawmode()
 {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
   {
     die("tcgetattr");
   }
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -68,10 +77,26 @@ char editorReadKey()
 
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+  {
+    return -1;
+  }
+  else 
+  {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 void editorDrawRows()
 {
   int y;
-  for (y = 0; y < 24; y++)
+  for (y = 0; y < E.screenrows; y++)
   {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
@@ -102,9 +127,19 @@ void editorProcessKeyPress()
 
 /*** init ***/
 
+void initEditor()
+{
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+  {
+    die("getWindowSize");
+  }
+}
+
+
 int main()
 {
   enableRawmode();
+  initEditor();
   
   while (1)
   {
