3ee0a2484034d0b4ec820ea838591ddd3b279760
Shriman <shrimanpro.gamer@gmail.com>
2025-12-25 11:24:04 -0700

    refactor: added a buffer to output text all at the same time

--- shrimp.c
+++ shrimp.c
@@ -4,6 +4,7 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
@@ -133,22 +134,57 @@ int getWindowSize(int *rows, int *cols)
   }
 }
 
-void editorDrawRows()
+struct abuf 
+{
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL)
+  {
+    return;
+  }
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+  free(ab->b);
+}
+
+// new change makes an extra tilde at end
+void editorDrawRows(struct abuf *ab)
 {
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    abAppend(ab, "~", 1);
+    if (y < E.screenrows - 1)
+    {
+      abAppend(ab, "\r\n", 2);
+    }
   }
 }
 
 void editorRefreshScreen()
 {
-  write(STDOUT_FILENO, "\x1b[2J",4);
-  write(STDOUT_FILENO, "\x1b[H",3);
+  struct abuf ab = ABUF_INIT;
+  abAppend(&ab, "\x1b[2J",4);
+  abAppend(&ab, "\x1b[H",3);
 
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[b", 3);
+  editorDrawRows(&ab);
+  abAppend(&ab, "\x1b[b", 3);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 void editorProcessKeyPress()
