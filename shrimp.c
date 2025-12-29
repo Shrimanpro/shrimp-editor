71c606be3e0e27d04b5ba69ed5d7bda2c97c3b0b
Shriman <shrimanpro.gamer@gmail.com>
2025-12-29 11:38:06 -0700

    feat: added future support for delete key

--- shrimp.c
+++ shrimp.c
@@ -22,7 +22,8 @@ enum editorKey
   PAGE_UP,
   PAGE_DOWN,
   HOME_KEY,
-  END_KEY
+  END_KEY,
+  DEL_KEY
 };
 
 /*** data ***/
@@ -116,6 +117,8 @@ int editorReadKey()
           {
             case '1':
               return HOME_KEY;
+            case '3':
+              return DEL_KEY;
             case '4':
               return END_KEY;
             case '5':
