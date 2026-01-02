603a829fc9c52f916a505bca3030b64531c578eb
Shriman <shrimanpro.gamer@gmail.com>
2026-01-01 18:16:01 -0700

    feat: added horizontal scrolling support to view long lines

--- shrimp.c
+++ shrimp.c
@@ -48,6 +48,7 @@ struct editorConfig
   int screencols;
   int numrows;
   int rowoff;
+  int coloff;
   erow *row;
   struct termios orig_termios;
 };
@@ -315,6 +316,14 @@ void editorScroll()
   {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff)
+  {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols)
+  {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
 
 // new change makes an extra tilde at end
@@ -355,12 +364,16 @@ void editorDrawRows(struct abuf *ab)
     }
     else 
     {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0)
+      {
+        len = 0;
+      }
       if (len > E.screencols)
       {
         len = E.screencols;
       }
-      abAppend(ab, E.row[filerow].chars, len);
+      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -382,7 +395,7 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -402,10 +415,7 @@ void editorMoveCursor(int key)
       }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols - 1)
-      {
-        E.cx++;
-      }
+      E.cx++;
       break;
     case ARROW_UP:
       if (E.cy != 0)
@@ -469,6 +479,7 @@ void initEditor()
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
