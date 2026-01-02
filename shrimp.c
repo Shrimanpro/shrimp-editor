6b841e483d864cc03dd786c3179559b61a82223a
Shriman <shrimanpro.gamer@gmail.com>
2026-01-01 18:10:41 -0700

    feat: added vertical scrolling to go through rest of the file

--- shrimp.c
+++ shrimp.c
@@ -47,6 +47,7 @@ struct editorConfig
   int screenrows;
   int screencols;
   int numrows;
+  int rowoff;
   erow *row;
   struct termios orig_termios;
 };
@@ -304,13 +305,26 @@ void abFree(struct abuf *ab)
   free(ab->b);
 }
 
+void editorScroll()
+{
+  if (E.cy < E.rowoff)
+  {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows)
+  {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+}
+
 // new change makes an extra tilde at end
 void editorDrawRows(struct abuf *ab)
 {
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    if (y >= E.numrows)
+    int filerow = y + E.rowoff;
+    if (filerow >= E.numrows)
     {
       if (E.numrows == 0 && y == E.screenrows / 3)
       {
@@ -341,12 +355,12 @@ void editorDrawRows(struct abuf *ab)
     }
     else 
     {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if (len > E.screencols)
       {
         len = E.screencols;
       }
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -359,6 +373,7 @@ void editorDrawRows(struct abuf *ab)
 
 void editorRefreshScreen()
 {
+  editorScroll();
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -367,7 +382,7 @@ void editorRefreshScreen()
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -399,7 +414,7 @@ void editorMoveCursor(int key)
       }
       break;
     case ARROW_DOWN:
-      if (E.cy != E.screenrows - 1)
+      if (E.cy < E.numrows)
       {
         E.cy++;
       }
@@ -453,6 +468,7 @@ void initEditor()
 {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
   E.numrows = 0;
   E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
