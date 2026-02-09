cb5beadccbe052eb5d69893ac0522d690b329b51
Shriman <shrimanpro.gamer@gmail.com>
2026-02-09 11:29:07 -0700

    feat: added modified on status bar

--- shrimp.c
+++ shrimp.c
@@ -58,6 +58,7 @@ struct editorConfig
   int coloff;
   int rx;
   erow *row;
+  int dirty;
   char *filename;
   char statusmsg[80];
   time_t statusmsg_time;
@@ -323,6 +324,7 @@ void editorAppendRow(char *s, size_t len)
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c)
@@ -336,6 +338,7 @@ void editorRowInsertChar(erow *row, int at, int c)
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 void editorInsertChar(int c)
@@ -395,6 +398,7 @@ void editorOpen(char *filename)
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 
 }
 
@@ -416,6 +420,7 @@ void editorSave()
       {
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk", len);
         return;
       }
@@ -539,7 +544,7 @@ void editorDrawStatusBar(struct abuf *ab)
 {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
-  int len = snprintf(status,  sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+  int len = snprintf(status,  sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
   if (len > E.screencols)
   {
@@ -749,6 +754,7 @@ void initEditor()
   E.numrows = 0;
   E.rx = 0;
   E.row = NULL;
+  E.dirty = 0;
   E.filename = NULL;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
