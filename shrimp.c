809ae0af3676453af691f1b34df6889c2b63e676
Shriman <shrimanpro.gamer@gmail.com>
2026-04-26 11:31:53 -0700

    feat: added support for enter key

--- shrimp.c
+++ shrimp.c
@@ -397,6 +397,25 @@ void editorInsertChar(int c)
   E.cx++;
 }
 
+void editorInsertNewline()
+{
+  if (E.cx == 0)
+  {
+    editorInsertRow(E.cy, "", 0);
+  }
+  else 
+  {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
 void editorDelChar()
 {
   if (E.cy == E.numrows)
@@ -741,7 +760,7 @@ void editorProcessKeyPress()
   switch (c) {
     
     case '\r':
-      // do something with enter key
+      editorInsertNewline();
       break;
 
     case BACKSPACE:
