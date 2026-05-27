0e9de90c3b39732c18f91a41d814b2e3c369ab71
Shriman <shrimanpro.gamer@gmail.com>
2026-05-26 19:23:22 -0700

    docs: added sample c code to test out syntaxing

---
+++ test.c
@@ -0,0 +1,15 @@
+// Exampel code to test out shrimp features
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // calculating sum
+    sum = num1 + num2;      
+    
+    printf("%d + %d = %d\n", num1, num2, sum);
+    return 0;
+}
