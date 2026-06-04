c5ad6df03ff6823b4e3d0da3c5e5ba094fbcadb9
Shriman <shrimanpro.gamer@gmail.com>
2026-06-03 17:32:20 -0700

    feat: added syntax highlighting for comments

--- shrimp.c
+++ shrimp.c
@@ -42,6 +42,7 @@ enum editorKey
 enum editorHighlight 
 {
   HL_NORMAL = 0,
+  HL_COMMENT,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH
@@ -54,6 +55,7 @@ struct editorSyntax
 {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -93,6 +95,7 @@ struct editorSyntax HLDB[] =
   {
     "c",
     C_HL_extensions,
+    "//",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
@@ -308,6 +311,9 @@ void editorUpdateSyntax(erow *row)
     return;
   }
 
+  char *scs = E.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int prev_sep = 1;
   int in_string = 0;
 
@@ -316,6 +322,15 @@ void editorUpdateSyntax(erow *row)
   {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (scs_len && !in_string)
+    {
+      if (!strncmp(&row->render[i], scs, scs_len))
+      {
+        memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+        break;
+      }
+    }
 
     if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
     {
@@ -376,6 +391,8 @@ int editorSyntaxToColor(int hl)
       return 34;
     case HL_STRING:
       return 36;
+    case HL_COMMENT:
+      return 33;
     default:
       return 37;
   }
