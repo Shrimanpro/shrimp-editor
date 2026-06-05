4030b648396047424578ab9357bb8f818af2b8dd
Shriman <shrimanpro.gamer@gmail.com>
2026-06-05 16:06:28 -0700

    fea: multiline comments should work

--- shrimp.c
+++ shrimp.c
@@ -43,6 +43,7 @@ enum editorHighlight
 {
   HL_NORMAL = 0,
   HL_COMMENT,
+  HL_MLCOMMENT,
   HL_KEYWORD1,
   HL_KEYWORD2,
   HL_STRING,
@@ -59,6 +60,8 @@ struct editorSyntax
   char **filematch;
   char **keywords;
   char *singleline_comment_start;
+  char *multiline_comment_start;
+  char *multiline_comment_end;
   int flags;
 };
 
@@ -106,7 +109,7 @@ struct editorSyntax HLDB[] =
     "c",
     C_HL_extensions,
     C_HL_keywords,
-    "//",
+    "//", "/*", "*/",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
@@ -325,10 +328,16 @@ void editorUpdateSyntax(erow *row)
   char **keywords = E.syntax->keywords;
 
   char *scs = E.syntax->singleline_comment_start;
+  char *mcs = E.syntax->multiline_comment_start;
+  char *mce = E.syntax->multiline_comment_end;
+ 
   int scs_len = scs ? strlen(scs) : 0;
+  int mcs_len = mcs ? strlen(mcs) : 0;
+  int mce_len = mce ? strlen(mce) : 0;
 
   int prev_sep = 1;
   int in_string = 0;
+  int in_comment = 0;
 
   int i = 0;
   while (i <row->rsize)
@@ -336,12 +345,40 @@ void editorUpdateSyntax(erow *row)
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-    if (scs_len && !in_string)
+    if (scs_len && !in_string && !in_comment)
     {
       if (!strncmp(&row->render[i], scs, scs_len))
       {
         memset(&row->hl[i], HL_COMMENT, row->rsize - i);
         break;
+      }
+    }
+
+    if (mcs_len && mce_len && !in_string)
+    {
+      if (in_comment)
+      {
+        row->hl[i] = HL_MLCOMMENT;
+        if (!strncmp(&row->render[i], mce, mce_len))
+        {
+          memset(&row->hl[i], HL_MLCOMMENT, mce_len);
+          i += mce_len;
+          in_comment = 0;
+          prev_sep = 1;
+          continue;
+        }
+        else 
+        {
+          i++;
+          continue;
+        }
+      }
+      else if (!strncmp(&row->render[i], mcs, mcs_len))
+      {
+        memset(&row->hl[i], HL_MLCOMMENT, mcs_len);
+        i += mcs_len;
+        in_comment = 1;
+        continue;
       }
     }
 
@@ -433,6 +470,7 @@ int editorSyntaxToColor(int hl)
     case HL_STRING:
       return 36;
     case HL_COMMENT:
+    case HL_MLCOMMENT:
       return 33;
     case HL_KEYWORD1:
       return 32;
